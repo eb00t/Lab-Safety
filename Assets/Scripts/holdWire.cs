f433d00f963710b9449d044a22bee286ab497f64
ImOmg <87231651+ImOmg@users.noreply.github.com>
2023-03-18 02:31:13 +0000

    wire mainly functional

--- Assets/Scripts/holdWire.cs
+++ Assets/Scripts/holdWire.cs
@@ -8,76 +8,76 @@ public class holdWire : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-
     public HingeJoint2D hj;
 
-    private bool attached = false;
-
-    public Transform attachedTo;
+    public GameObject handle;
     public GameObject player;
-    //private GameObject disregard;
-    // Start is called before the first frame update
+    private SpringJoint2D springJoint;
 
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         hj = gameObject.GetComponent<HingeJoint2D>();
+       // sj = gameObject.GetComponent<SpringJoint2D>();
+
+        /* if (!springJoint)
+         {
+             GameObject go = new GameObject("Rigidbody2D Dragger");
+             Rigidbody2D body = go.AddComponent<Rigidbody2D>() as Rigidbody2D;
+             springJoint = go.AddComponent<SpringJoint2D>() as SpringJoint2D;
+             //Rigidbody2D body = go.AddComponent ("Rigidbody2D") as Rigidbody2D;
+             //springJoint = go.AddComponent ("SpringJoint2D") as SpringJoint2D;
+             body.isKinematic = true;
+         }*/
+
     }
 
     void Start()
     {
-        
+        rb.isKinematic = true;
     }
-
-    // Update is called once per frame
-    void Update(Rigidbody2D wireBone)
+    
+    void Update()
     {
-        
         if (Input.GetKey("q"))
         {
-            wireBone.gameObject.GetComponent<WireSegments>().isPlayerAttached= true;
-            hj.connectedBody = wireBone;
-            hj.enabled = true;
-            attached = true;
-            attachedTo = wireBone.gameObject.transform.parent;
-            //attachedTo.transform.parent = player.transform;
-            // Attach(gameObject.GetComponent<Rigidbody2D>());
-        }
-        else
-        {
-            hj.connectedBody.gameObject.GetComponent<WireSegments>().isPlayerAttached = false;
-            attached = false;
-            hj.enabled = false;
-            hj.connectedBody = null;
-            //attachedTo.transform.parent = null;
-        }
-        
-    }
-
-   /* public void Attach(Rigidbody2D wireBone)
-    {
-        wireBone.gameObject.GetComponent<WireSegments>().isPlayerAttached= true;
-        hj.connectedBody = wireBone;
-        hj.enabled = true;
-        attached = true;
-        attachedTo = wireBone.gameObject.transform.parent;
-    }
-
-    public void Detach()
-    {
-        hj.connectedBody
-    }
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        
-        if (Input.GetKey("q"))
-        {
+            handle.transform.position = hj.transform.position;
+            player.transform.parent = hj.transform;
+            rb.isKinematic = true;
+            //player.transform.parent = handle.transform;
             
         }
         else
         {
-            Detach();
+            player.transform.parent = null;
+            hj.transform.parent = null;
+            rb.isKinematic = false;
         }
-        
-    }*/
+    }
+
+    public void Attach()
+    {
+      
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        /*if (col.gameObject.CompareTag("Wire") )
+        {
+            if (Input.GetKey("q"))
+        {
+            handle.transform.position = hj.transform.position;
+            player.transform.parent = hj.transform;
+            rb.isKinematic = true;
+            //player.transform.parent = handle.transform;
+            
+        }
+        else
+        {
+            player.transform.parent = null;
+            hj.transform.parent = null;
+            rb.isKinematic = false;
+        }
+        }*/
+    }
 }
