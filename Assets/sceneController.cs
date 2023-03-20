b306b74a58dec64da747384fd26ecea51318a3f7
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2023-03-20 14:28:58 +0000

    Fixed inventory and added game screens

---
+++ Assets/sceneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT GAME!");
+    }
+
+    public void replayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
