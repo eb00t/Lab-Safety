54056ac3171c29eae5229b238ec2d24b68edc004
ImOmg <87231651+ImOmg@users.noreply.github.com>
2023-03-21 01:25:26 +0000

    systems, im tired
    Revert "minor fix"
    
    This reverts commit 5c3578cdc9404f22bf7f35e1f297d4530c2556d5.
    
    plz

--- Assets/Scripts/ScienceJuiceScripts/RedSJScript.cs
+++ Assets/Scripts/ScienceJuiceScripts/RedSJScript.cs
@@ -6,17 +6,21 @@ using UnityEngine;
 public class RedSJScript : MonoBehaviour
 {
     public GameObject player;
+    public GameObject[] enemy;
     public Transform respawnPoint;
-    //public bool burn, overCharge, explode, scienceJ;
 
+    public GameObject fireball,boomB;
+    //public bool burn, overCharge, explode, scienceJ;=
+     
     public RedSJ effects;
 
-    private Animator anim;
+    private List<Animator> anim;
     
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator[]>();
+       // anim1 = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,26 +41,28 @@ public class RedSJScript : MonoBehaviour
             switch (effects)
             {
                 case RedSJ.nothing:
+                    // ¯\_(ツ)_/¯
                     break;
                 
                 case RedSJ.burnable:
-                    Destroy(gameObject, 2.0f);
+                    StartCoroutine(burn());
+                    //burnnin();
                     break;
                 
                 case RedSJ.overcharge:
+                    overCharge();
                     break;
                 
                 case RedSJ.explode:
-                    //temp explosion
-                    Destroy(gameObject, 2f);
+                    StartCoroutine(boom());
+                   //boomBoom();
                     break;
                 
                 case  RedSJ.mixSJ:
                     break;
                 
-                case RedSJ.playerRed:
-                    player.transform.position = respawnPoint.position;
-                    Debug.Log("im working");
+                case RedSJ.death:
+                    StartCoroutine(death());
                     break;
                 default: break;
             }
@@ -89,9 +95,48 @@ public class RedSJScript : MonoBehaviour
         }
     }*/
 
-    private void death()
+    IEnumerator death()
     {
-        player.transform.position = respawnPoint.position;
+        
+        if (gameObject.CompareTag("Enemy"))
+        {
+            foreach (GameObject obj in enemy)
+            {
+                obj.SetActive(false);
+            }
+            
+            yield return new WaitForSeconds(.01f);
+        }
+        
+        if (gameObject.CompareTag("Player"))
+        {
+            yield return new WaitForSeconds(1f);
+            player.transform.position = respawnPoint.position;
+            Debug.Log("im working");
+        }
+    }
+    
+
+    IEnumerator burn()
+    {
+
+        fireball.SetActive(true);
+       /// anim.Play("fireA");
+        yield return new WaitForSeconds (7f);
+        gameObject.SetActive(false);
+    }
+    
+
+    IEnumerator boom()
+    {
+        boomB.SetActive(true);
+        //anim1.Play("explodeTempA");
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
     }
 
+    private void overCharge()
+    {
+        
+    }
 }
