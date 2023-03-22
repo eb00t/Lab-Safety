5a7017784468ec851b2d6a6aab4c128d865f1029
ImOmg <87231651+ImOmg@users.noreply.github.com>
2023-03-22 10:14:21 +0000

    playtest

---
+++ Assets/Scripts/batteryScript.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batteryScript : MonoBehaviour
+{
+    public GameObject player, battery,wall;
+    public GameObject red;
+
+    public bool IsCharged;
+   // public RedSJScript batCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //player.transform.position = respawnPoint.position;
+            gameObject.transform.parent = player.transform;
+        }
+
+        if (col.gameObject.CompareTag("Blue"))
+        {
+            IsCharged = true;
+            
+            
+        }
+
+        if (IsCharged == true)
+        {
+            if (col.gameObject.CompareTag("Charger"))
+            {
+                wall.SetActive(false);
+                red.SetActive(true);
+            }
+        }
+
+        
+    }
+}
