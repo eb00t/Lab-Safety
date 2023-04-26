657c50ac3bc204c59e8cef6a3360e6ea92b5b47c
ImOmg <87231651+ImOmg@users.noreply.github.com>
2023-04-26 06:26:47 +0100

    playtest ready

--- Assets/Scripts/ScienceJuiceScripts/SJitems/Activation.cs
+++ Assets/Scripts/ScienceJuiceScripts/SJitems/Activation.cs
@@ -6,37 +6,39 @@ using UnityEngine;
 public class Activation : MonoBehaviour
 {
     //public GameObject activatedObject;
-    public bool isActivated;
-    public batteryScript batteryisCharged;
+    [SerializeField] private bool isActivated;
+    private batteryScript batteryisCharged;
+    private GameObject bat;
     private Collider2D chargerCollider;
     public Collider2D SJCollider;
+    private Transform Battery;
 
     // Start is called before the first frame update
     void Start()
     {
         SJCollider.enabled = false;
         chargerCollider = GetComponent<Collider2D>();
+        bat = GameObject.FindGameObjectWithTag("Charger");
+        Battery = GameObject.FindGameObjectWithTag("Charger").transform;
         isActivated = false;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Battery") && col.transform.IsChildOf(transform))
+        if (col.gameObject.CompareTag("Battery") && Battery.transform.IsChildOf(transform))
         {
             Debug.Log("activate");
             chargerCollider.enabled = !chargerCollider.enabled;
             isActivated = true;
-        }
-
-        if (isActivated)
-        {
             SJCollider.enabled = true;
         }
 
-        if (batteryisCharged.IsCharged)
+        /*if (isActivated)
         {
-            
-        }
+            SJCollider.enabled = true;
+        }*/
+
+      
         
        /* if (col.gameObject.CompareTag("Battery"))
         {
