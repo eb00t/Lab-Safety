88d6617cfa99d3296a54d87ea682950e11610225
erikwtn <erik.wtn@gmail.com>
2023-04-29 21:17:29 +0100

    generator (green puddle) now changes size based on what hits it

--- Assets/Scripts/ScienceJuiceScripts/greenSJScript.cs
+++ Assets/Scripts/ScienceJuiceScripts/greenSJScript.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class greenSJScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 originalSize;
+    private void Start()
     {
-        
+        originalSize = transform.parent.transform.localScale;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("object detected");
+        if (other.CompareTag("YellowSJ")) // quadruple
+        {
+            StartCoroutine(ResizeObject(new Vector3(originalSize.x, originalSize.y * 4, originalSize.z)));
+        }
+        else if (other.CompareTag("RedSJ")) // reset to 0
+        { 
+            Debug.Log("Object was red");
+            StartCoroutine(ResizeObject(originalSize));
+        }
+        else if (other.CompareTag("BlueSJ")) // double
+        {
+            StartCoroutine(ResizeObject(new Vector3(originalSize.x, originalSize.y * 2, originalSize.z)));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ResizeObject(Vector3 newScale)
     {
-        
+        float currentTime = 0.0f;
+
+        while (currentTime < 2f)
+        {
+            transform.parent.transform.localScale = Vector3.Lerp(transform.parent.transform.localScale, newScale, currentTime / 2f);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.parent.transform.localScale = newScale;
     }
 }
